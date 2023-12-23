0960b3d02f916e99cd0f37b51c13d7273ecec371
lana-z <lana@levelupeconomy.com>
2023-12-22 23:42:48 -0800

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import beasts from './beasts.json';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './src/app.css';
+import './src/App.css';
 
 
 function App(){
